3ce011592775573fde080241977434e0eb258ba6
Lucas Jones <lucasarchboldjones@gmail.com>
2015-12-05 20:00:55 +0000

    Removed all uses of 'std::'

--- RedditSolution/RedditSolution.cpp
+++ RedditSolution/RedditSolution.cpp
@@ -1,21 +1,24 @@
+#include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
 
+using namespace std;
+
 //Random comment
 struct Person {
-	std::string name, surname, telephone, email;
+	string name, surname, telephone, email;
 };
 
-std::ostream& operator<<(std::ostream &out, const Person &p) {
+ostream& operator<<(ostream &out, const Person &p) {
 	out << p.name << ' ' << p.surname << ": " << p.telephone << " " << p.email;
 	return out;
 }
 
 int main() {
-	std::stringstream ss{ "<person S Smith N Tom >\n<person S Smith N Dick T +49.921.1434 >\n<person S Smith N Harry E hsmith@gmail.com >\n<person S Smith N Mary T +39.921.1434 E msmith@gmail.com >\n<person S John N John T +33.921.1434 E jjohn@gmail.com >" };
-	std::string tmp;
-	std::vector<Person> people;
+	stringstream ss{ "<person S Smith N Tom >\n<person S Smith N Dick T +49.921.1434 >\n<person S Smith N Harry E hsmith@gmail.com >\n<person S Smith N Mary T +39.921.1434 E msmith@gmail.com >\n<person S John N John T +33.921.1434 E jjohn@gmail.com >" };
+	string tmp;
+	vector<Person> people;
 
 	while (ss >> tmp) {
 		if (tmp == "<person") {
@@ -35,18 +38,18 @@ int main() {
 					ss >> p.email;
 				}
 				else {
-					std::cerr << "Unknown token: " << tmp << "\n";
+					cerr << "Unknown token: " << tmp << "\n";
 				}
 				ss >> tmp;
 			}
 			people.push_back(p);
 		}
 		else {
-			std::cerr << "Bad starting sequence: " << tmp << std::endl;
+			cerr << "Bad starting sequence: " << tmp << endl;
 		}
 	}
 
 	for (auto &p : people) {
-		std::cout << p << "\n";
+		cout << p << "\n";
 	}
 }
